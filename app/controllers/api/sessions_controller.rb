2a73970fca0d88dbac024977e90af8f4057c29e0
Maxim Grebennikov <clustermass2@gmail.com>
2018-06-01 15:12:24 -0700

    Initial commit

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,23 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      render :create
+    else
+      # flash.now[:errors] = ["Invalid username or password"]
+      render json: {errors:{'Invalid username or password'}}, status: 422
+    end
+  end
+
+  def destroy
+    if !current_user
+    render json: {errors:{'404 No active session'}}, status: 404
+    else
+    logout
+    render json: {}
+    end
+  end
+
+end
